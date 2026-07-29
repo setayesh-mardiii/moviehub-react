694d13269da2b369a76a0445b3e7a2b792875c98
Seta.mardi02 <mardi.seti02@gmail.com>
2026-07-29 19:27:46 +0330

    create top rated movies page

--- src/pages/TopRated/TopRated.jsx
+++ src/pages/TopRated/TopRated.jsx
@@ -1,13 +1,72 @@
+import "./TopRated.css";
+
+import MovieCard from "../../components/MovieCard/MovieCard";
+
+import { topRatedMovies } from "../../Data/topRatedMovies";
+
+
+
 function TopRated() {
 
+
   return (
-    <div style={{paddingTop:"100px"}}>
-      <h1>
-        Top Rated Page Works
-      </h1>
-    </div>
+
+    <section className="top-rated-page">
+
+
+      <div className="top-rated-container">
+
+
+        <div className="top-rated-header">
+
+
+          <h1>
+            فیلم‌های برتر
+          </h1>
+
+
+          <p>
+            مجموعه‌ای از بالاترین امتیازهای سینما
+          </p>
+
+
+        </div>
+
+
+
+
+
+        <div className="movies-grid">
+
+
+          {
+            topRatedMovies.map((movie)=>(
+
+
+              <MovieCard
+
+                key={movie.id}
+
+                movie={movie}
+
+              />
+
+
+            ))
+          }
+
+
+        </div>
+
+
+      </div>
+
+
+    </section>
+
   );
 
 }
+
 
 export default TopRated;
