99a5d76e6daf95f6941bb7c7779d3936e02c1f0d
Seta.mardi02 <mardi.seti02@gmail.com>
2026-07-30 15:40:50 +0330

    Add Vazirmatn font to project

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,9 @@ import { ThemeProvider } from "./context/ThemeContext.jsx";
 
 import "./index.css";
 import "./styles/Variable.css";
+import "@fontsource/vazirmatn/400.css";
+import "@fontsource/vazirmatn/500.css";
+import "@fontsource/vazirmatn/700.css";
 
 
 createRoot(document.getElementById("root")).render(
