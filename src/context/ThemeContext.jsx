1a9ecbfa2c47282a890e99e235c5769d48bb64a4
Seta.mardi02 <mardi.seti02@gmail.com>
2026-08-02 20:32:07 +0330

    clean project remove unused api token setup

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -8,43 +8,96 @@ export const ThemeContext = createContext(null);
 export function ThemeProvider({ children }) {
 
 
-  const [darkMode, setDarkMode] = useState(true);
+  const [darkMode, setDarkMode] = useState(() => {
+
+
+    const savedTheme = localStorage.getItem("theme");
+
+
+    if (savedTheme !== null) {
+
+
+      try {
+
+        return JSON.parse(savedTheme);
+
+      } catch {
+
+        return savedTheme === "dark";
+
+      }
+
+
+    }
+
+
+    return true; // حالت پیش‌فرض دارک
+
+
+
+  });
 
 
 
   useEffect(() => {
 
+
     document.body.classList.toggle("dark", darkMode);
 
     document.body.classList.toggle("light", !darkMode);
+
+
+
+    localStorage.setItem(
+
+      "theme",
+
+      JSON.stringify(darkMode)
+
+    );
+
 
   }, [darkMode]);
 
 
 
+
+
   const toggleTheme = () => {
 
+
     setDarkMode(prev => !prev);
+
 
   };
 
 
 
+
+
   return (
+
 
     <ThemeContext.Provider
 
       value={{
+
         darkMode,
+
         toggleTheme
+
       }}
 
     >
 
+
       {children}
+
 
     </ThemeContext.Provider>
 
+
   );
+
 
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,146 +2,71 @@ import "./Home.css";
 
 import { useState } from "react";
 
-
 import HeroSlider from "../../components/HeroSlider/HeroSlider";
 import TrendingShowcase from "../../components/TrendingShowcase/TrendingShowcase";
 import MovieSection from "../../components/MovieSection/MovieSection";
 import Genres from "../../components/Genres/Genres";
-
 
 import { movies } from "../../Data/Movies";
 import { latestMovies } from "../../Data/latestMovies";
 import { topRatedMovies } from "../../Data/topRatedMovies";
 
 
-
-
 function Home() {
-
-
 
   const [activeMovie, setActiveMovie] = useState(null);
 
 
-
-
-
   return (
 
-
-
     <div className="home">
-
-
-
-
 
       <HeroSlider />
 
 
-
-
-
-
       <main className="home-container">
-
-
-
-
 
         <TrendingShowcase />
 
 
-
-
-
-
-
-
         <MovieSection
-
           title="Popular Movies"
-
           movies={movies}
-
           activeMovie={activeMovie}
-
           setActiveMovie={setActiveMovie}
-
           sectionClass="popular-section"
-
         />
 
 
-
-
-
-
-
-
-
         <MovieSection
-
           title="Latest Movies"
-
           movies={latestMovies}
-
           activeMovie={activeMovie}
-
           setActiveMovie={setActiveMovie}
-
           sectionClass="latest-section"
-
         />
-
-
-
-
-
-
-
 
 
         <MovieSection
-
           title="Top Rated Movies"
-
           movies={topRatedMovies}
-
           activeMovie={activeMovie}
-
           setActiveMovie={setActiveMovie}
-
           sectionClass="top-rated-section"
-
         />
-
-
-
-
-
-
 
 
         <Genres />
 
 
-
-
-
       </main>
-
-
-
 
 
     </div>
 
-
   );
 
 }
-
 
 
 export default Home;
