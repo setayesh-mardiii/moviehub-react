b5e34d4a08ccf1543e23c35d1fbc0ad664684bf3
Seta.mardi02 <mardi.seti02@gmail.com>
2026-08-06 21:19:29 +0330

    fix movie details favorite button style and light mode

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -1,4 +1,5 @@
 import { useParams } from "react-router-dom";
+import { useState } from "react";
 
 import {
   FaStar,
@@ -7,349 +8,110 @@ import {
   FaClock,
   FaPlay,
   FaHeart,
-  FaUsers
+  FaUsers,
 } from "react-icons/fa";
 
 import movies from "../../Data/movies.json";
 
 import "./MovieDetails.css";
 
-
 function MovieDetails() {
-
-
+  const [isFavorite, setIsFavorite] = useState(false);
   const { id } = useParams();
 
-
-
-  const movie = movies.find(
-    item => item.id === Number(id)
-  );
-
-
-
-
+  const movie = movies.find((item) => item.id === Number(id));
 
   if (!movie) {
-
     return (
-
-      <div
-        className="movie-details"
-        dir="rtl"
-      >
-
-        <h1>
-          فیلم پیدا نشد
-        </h1>
-
+      <div className="movie-details" dir="rtl">
+        <h1>فیلم پیدا نشد</h1>
       </div>
-
     );
-
   }
 
-
-
-
-
-
-
   return (
-
-
-    <div
-      className="movie-details"
-      dir="rtl"
-    >
-
-
-
-
-
+    <div className="movie-details" dir="rtl">
       <div className="details-shape">
-
-
-
-
-
-        {/* BLOB BACKGROUND */}
-
         <div className="blob-bg"></div>
-
-
-
-
-
-
 
         {/* POSTER */}
 
-
         <div className="poster-area">
-
-
-          <img
-
-            src={movie.image}
-
-            alt={movie.title}
-
-          />
-
-
+          <img src={movie.image} alt={movie.title} />
         </div>
-
-
-
-
-
-
-
-
 
         {/* INFORMATION */}
 
-
-
         <div className="info-area">
-
-
-
-          <h1>
-
-            {movie.title}
-
-          </h1>
-
-
-
-
-
-
+          <h1>{movie.title}</h1>
 
           <div className="meta-pills">
-
-
-
             <span>
-
               <FaStar />
 
               {movie.rating}
-
             </span>
 
-
-
-
-
             <span>
-
               <FaCalendarAlt />
 
               {movie.year}
-
             </span>
 
-
-
-
-
             <span>
-
               <FaClock />
 
               {movie.duration}
-
             </span>
 
-
-
-
-
             <span>
-
               <FaFilm />
 
               {movie.genre}
-
             </span>
-
-
-
           </div>
 
-
-
-
-
-
-
-
-
-          <p className="description">
-
-
-            {movie.description}
-
-
-          </p>
-
-
-
-
-
-
-
-
+          <p className="description">{movie.description}</p>
 
           <div className="action-buttons">
-
-
-
             <button className="watch-btn">
-
-
               <FaPlay />
-
               تماشا
-
-
             </button>
-
-
-
-
-
-
 
             <button className="trailer-btn">
-
-
               <FaFilm />
-
               تریلر
-
-
             </button>
 
+            {/* FAVORITE */}
 
-
-
-
-
-
-
-            <button className="favorite-btn">
-
-
+            <button
+              className={`details-favorite-btn ${isFavorite ? "active" : ""}`}
+              onClick={() => setIsFavorite(!isFavorite)}
+            >
               <FaHeart />
-
-
             </button>
-
-
-
           </div>
 
-
-
-
-
-
-
-
-
           <section className="actors">
-
-
-
             <h2>
-
               <FaUsers />
-
               بازیگران
-
             </h2>
 
-
-
-
-
-
-
             <div className="actor-list">
-
-
-
-              {
-
-                movie.actors?.map(
-
-                  (actor,index)=>(
-
-
-                    <span
-
-                      key={index}
-
-                      className="actor-pill"
-
-                    >
-
-                      {actor}
-
-                    </span>
-
-
-                  )
-
-                )
-
-              }
-
-
-
+              {movie.actors?.map((actor, index) => (
+                <span key={index} className="actor-pill">
+                  {actor}
+                </span>
+              ))}
             </div>
-
-
-
-
-
-
           </section>
-
-
-
-
-
-
-
-
         </div>
-
-
-
-
-
-
-
       </div>
-
-
-
-
-
-
-
     </div>
-
-
   );
-
 }
-
-
 
 export default MovieDetails;
